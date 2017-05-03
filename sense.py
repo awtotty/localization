b581462f2e29578b1bfe73f255dc66fdb950f88d
Austin <awtotty@gmail.com>
2017-05-02 20:05:56 -0700

    Sense move cycle

--- sense.py
+++ sense.py
@@ -1,9 +1,18 @@
+#Given the list motions=[1,1] which means the robot
+#moves right and then right again, compute the posterior
+#distribution if the robot first senses red, then moves
+#right one, then senses green, then moves right again,
+#starting with a uniform prior distribution.
 
 p=[0.2, 0.2, 0.2, 0.2, 0.2]
 world=['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
+motions = [1,1]
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
 def sense(p, Z):
     q=[]
@@ -16,8 +25,16 @@ def sense(p, Z):
     return q
 
 def move(p, U):
-    U = U % len(p)
-    q = p[-U:] + p[:-U]
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
     return q
 
-print move(p, 1)
+for motion, measurement in zip(motions,measurements):
+    p = sense(p, measurement)
+    p = move(p, motion)
+
+print p         
